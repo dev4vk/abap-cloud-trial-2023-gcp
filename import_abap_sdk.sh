1e9cb2832504164b18bb863d597bbf7598be402b
dev4vk <vinothkumarit@zoho.com>
2025-07-21 22:50:06 +0530

    Update import_abap_sdk.sh

--- import_abap_sdk.sh
+++ import_abap_sdk.sh
@@ -1,26 +1,22 @@
 #!/bin/bash
-#Create directory and download transport
+# Create directory and download latest transport (v1.11)
 mkdir abap_sdk_transport
 cd abap_sdk_transport
-wget  https://storage.googleapis.com/cloudsapdeploy/connectors/abapsdk/abap-sdk-for-google-cloud-1.8.zip
+wget https://storage.googleapis.com/cloudsapdeploy/connectors/abapsdk/abap-sdk-for-google-cloud-1.11.zip
 
-#Unzip the transport files
-unzip abap-sdk-for-google-cloud-1.8.zip
+# Unzip the transport files
+unzip abap-sdk-for-google-cloud-1.11.zip
 
-# Copy transport files into the Docker container
-sudo docker cp K900319.GM1 a4h:/usr/sap/trans/cofiles/K900319.GM1
-sudo docker cp R900319.GM1 a4h:/usr/sap/trans/data/R900319.GM1
-sudo docker cp K900321.GM1 a4h:/usr/sap/trans/cofiles/K900321.GM1
-sudo docker cp R900321.GM1 a4h:/usr/sap/trans/data/R900321.GM1
+# Copy transport files into the Docker container (adjust file names as needed)
+sudo docker cp K900457.GM1 a4h:/usr/sap/trans/cofiles/K900457.GM1
+sudo docker cp R900457.GM1 a4h:/usr/sap/trans/data/R900457.GM1
 
-# Set correct permissions for the copied files within the container
-sudo docker exec -it a4h chown a4hadm:sapsys /usr/sap/trans/cofiles/K900319.GM1
-sudo docker exec -it a4h chown a4hadm:sapsys /usr/sap/trans/data/R900319.GM1
-sudo docker exec -it a4h chown a4hadm:sapsys /usr/sap/trans/cofiles/K900321.GM1
-sudo docker exec -it a4h chown a4hadm:sapsys /usr/sap/trans/data/R900321.GM1
+# If you also need the OAuth sample transport (optional), repeat for K900458 / R900458
 
-# Add transports to buffer and import them
-sudo docker exec -it a4h runuser -l a4hadm -c 'tp addtobuffer GM1K900319 A4H client=001 pf=/usr/sap/trans/bin/TP_DOMAIN_A4H.PFL'
-sudo docker exec -it a4h runuser -l a4hadm -c 'tp pf=/usr/sap/trans/bin/TP_DOMAIN_A4H.PFL import GM1K900319 A4H U128 client=001'
-sudo docker exec -it a4h runuser -l a4hadm -c 'tp addtobuffer GM1K900321 A4H client=001 pf=/usr/sap/trans/bin/TP_DOMAIN_A4H.PFL'
-sudo docker exec -it a4h runuser -l a4hadm -c 'tp pf=/usr/sap/trans/bin/TP_DOMAIN_A4H.PFL import GM1K900321 A4H U128 client=001'
+# Set correct permissions
+sudo docker exec -it a4h chown a4hadm:sapsys /usr/sap/trans/cofiles/K900457.GM1
+sudo docker exec -it a4h chown a4hadm:sapsys /usr/sap/trans/data/R900457.GM1
+
+# Add to buffer and import
+sudo docker exec -it a4h runuser -l a4hadm -c 'tp addtobuffer GM1K900457 A4H client=001 pf=/usr/sap/trans/bin/TP_DOMAIN_A4H.PFL'
+sudo docker exec -it a4h runuser -l a4hadm -c 'tp pf=/usr/sap/trans/bin/TP_DOMAIN_A4H.PFL import GM1K900457 A4H U128 client=001'
